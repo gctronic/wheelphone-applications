2d476f5312d1b584341ad9d98c7992735e447c18
gnicolax@gmail.com <gnicolax@gmail.com@69f22f91-4d63-1a31-ada6-6e98f0020068>
2013-07-08 14:03:51 +0000

    NV Wheelphone_faceme. An app that makes the robot always face the first detected human face

---
+++ Wheelphone_faceme/src/com/wheelphone/faceme/ActivityMain.java
@@ -0,0 +1,110 @@
+package com.wheelphone.faceme;
+
+import android.app.ActionBar;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+
+import com.wheelphone.wheelphonelibrary.WheelphoneRobot;
+
+public class ActivityMain extends FragmentActivity implements
+ActionBar.OnNavigationListener {
+
+	private static String TAG = ActivityMain.class.getName();
+
+	private FragmentFaceme fragmentFaceme;
+	
+	/**
+	 * The serialization (saved instance state) Bundle key representing the
+	 * current dropdown position.
+	 */
+	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Set up the action bar to show a dropdown list.
+		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+		// Set up the dropdown list navigation in the action bar.
+		actionBar.setListNavigationCallbacks(
+				// Specify a SpinnerAdapter to populate the dropdown list.
+				new ArrayAdapter<String>(actionBar.getThemedContext(),
+						android.R.layout.simple_list_item_1,
+						android.R.id.text1, new String[] {
+					getString(R.string.faceme),
+					getString(R.string.about), }), this);
+
+		//Make sure that the app stays open:
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
+				WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+				WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+				WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+		try {
+			PackageManager manager = this.getPackageManager();
+			PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+			Log.d(TAG, "Info:" + info.packageName + "\n" + info.versionCode + "\n" + info.versionName); 
+		} catch (NameNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		// Restore the previously serialized current dropdown position.
+		if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+			getActionBar().setSelectedNavigationItem(
+					savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+		}
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// Serialize the current dropdown position.
+		outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getActionBar()
+				.getSelectedNavigationIndex());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int position, long id) {
+		// When the given dropdown item is selected, show its contents in the
+		// container view.
+		Fragment fragment = null;
+
+		switch (position){
+		case 0:
+			if (fragmentFaceme == null)
+				fragmentFaceme = new FragmentFaceme();
+			fragment = fragmentFaceme;
+			break;
+		case 1:
+			fragment = new FragmentAbout();
+			break;
+		}
+
+		getSupportFragmentManager().beginTransaction()
+		.replace(R.id.container, fragment).commit();
+		return true;
+	}
+}
---
+++ Wheelphone_faceme/src/com/wheelphone/faceme/FragmentFaceme.java
@@ -0,0 +1,249 @@
+package com.wheelphone.faceme;
+
+import java.io.IOException;
+
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Face;
+import android.hardware.Camera.FaceDetectionListener;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wheelphone.wheelphonelibrary.WheelphoneRobot;
+
+public class FragmentFaceme extends Fragment implements SurfaceHolder.Callback{
+
+	private static String TAG = FragmentFaceme.class.getName();
+
+	private Camera camera;
+	private SurfaceView surfaceView;
+	private SurfaceHolder surfaceHolder;
+	private boolean previewing = false;
+	private int leftSpeed = 0;
+	private int rightSpeed = 0;
+	private int currDesiredHeight = 0;
+
+	private TextView output;
+
+	private WheelphoneRobot wheelphone;
+	
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_faceme,
+				container, false);
+		output = (TextView)rootView.findViewById(R.id.output);
+
+		if (setCamera()){
+			surfaceView = (SurfaceView)rootView.findViewById(R.id.camerapreview);
+			surfaceHolder = surfaceView.getHolder();
+			surfaceHolder.addCallback(this);
+		}
+
+		//Start robot control:
+		wheelphone = new WheelphoneRobot(getActivity(), getActivity());
+		wheelphone.startUSBCommunication();
+		wheelphone.enableSoftAcceleration();
+		wheelphone.enableSpeedControl();
+		return rootView;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		wheelphone.resumeUSBCommunication();
+	}
+
+	@Override
+	public void onPause() {		
+		//Stop robot before disconnecting:
+		leftSpeed = 0;
+		rightSpeed = 0;
+		currDesiredHeight = 0;
+		wheelphone.setSpeed(leftSpeed, rightSpeed);
+
+		wheelphone.pauseUSBCommunication();
+		super.onPause();
+	}
+
+	public void updateOutput() {
+		if (output != null){
+			String status = wheelphone.isUSBConnected() ? "Connected" : "Disconnected";
+			output.setText(status + ". L: " + leftSpeed + ", R: " + rightSpeed);
+		}
+	}
+
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		if(previewing){
+			camera.stopFaceDetection();
+			camera.stopPreview();
+			previewing = false;
+		}
+
+		if (camera != null){
+			try {			    
+				camera.setPreviewDisplay(surfaceHolder);
+				camera.startPreview();
+
+				camera.setDisplayOrientation(90);
+
+				//TODO: Pick dynamically the best preview size.
+
+				camera.startFaceDetection();
+				previewing = true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+//	    getCamera(); already got camera when we checked for the required features
+	    
+		camera.setFaceDetectionListener(faceDetectionListener);
+
+		//		Camera.Parameters param = camera.getParameters();
+
+		//		Log.d(TAG, "preferred height: " + param.getPreferredPreviewSizeForVideo().height); 
+		//		Log.d(TAG, "preferred width: " + param.getPreferredPreviewSizeForVideo().width); 
+		//		Log.d(TAG, "surf width: " + holder.getSurfaceFrame().width()); 
+		//		Log.d(TAG, "surf height: " + holder.getSurfaceFrame().height());
+		//		Log.d(TAG, "prev height: " + param.getPreviewSize().height); 
+		//		Log.d(TAG, "prev width: " + param.getPreviewSize().width); 
+
+		//		param.setPreviewSize(param.getPreferredPreviewSizeForVideo().width, param.getPreferredPreviewSizeForVideo().height);
+
+		//		Log.d(TAG, "prev height: " + param.getPreviewSize().height); 
+		//		Log.d(TAG, "prev width: " + param.getPreviewSize().width); 
+
+		//		List<int[]> fpslist = param.getSupportedPreviewFpsRange();
+		//		Log.d(TAG, "size= " + fpslist.size());
+		//		for (int i=0;i < fpslist.size();i++) {
+		//			fpslist.get(i);
+		//			Log.d(TAG, i + " fps= " + fpslist.get(i)[Camera.Parameters.PREVIEW_FPS_MIN_INDEX]);
+		//			Log.d(TAG, i + " fps= " + fpslist.get(i)[Camera.Parameters.PREVIEW_FPS_MAX_INDEX]);
+		//		}
+
+		//		camera.setParameters(param);
+	}
+
+	
+	private boolean setCamera() {
+		CameraInfo ci = new CameraInfo();
+	    for (int i = 0 ; i < Camera.getNumberOfCameras(); i++) {
+	        Camera.getCameraInfo(i, ci);
+	        if (ci.facing == CameraInfo.CAMERA_FACING_FRONT) {
+	        	camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT);
+	        } 
+	    }
+	    if (camera == null){
+	    	for (int i = 0 ; i < Camera.getNumberOfCameras(); i++) {
+	    		Camera.getCameraInfo(i, ci);
+	    		if (ci.facing == CameraInfo.CAMERA_FACING_BACK) {
+	    			camera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+	    		}
+	    	}
+	    }
+	    //If couldn't find a camera show error
+	    if (camera == null){
+	    	output.setText(getString(R.string.error_no_camera));
+	    	Log.e(TAG, "No camera available!");
+	    	return false;
+	    }
+		//Camera does not support face detection, show error
+		if (camera.getParameters().getMaxNumDetectedFaces() < 1){
+			Log.e(TAG, "No face detection available!");
+			output.setText(getString(R.string.error_no_facedetection));
+			return false;
+		}
+	    return true;
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		camera.stopFaceDetection();
+		camera.stopPreview();
+		camera.release();
+		camera = null;
+		previewing = false;
+	}
+
+	FaceDetectionListener faceDetectionListener = new FaceDetectionListener(){
+		@Override
+		public void onFaceDetection(Face[] faces, Camera camera) {
+			Log.d(TAG, "Faces detected: " + faces.length);
+			if (faces.length > 0){
+				if (currDesiredHeight == 0){
+					currDesiredHeight = faces[0].rect.height();
+				}
+				
+				// Face coordinates range from (-1000, 1000). Scale it to -64 to 64 (robot supports -127 to 127, but that is too fast and can easily overshoot):
+				int faceYPos = 32 * faces[0].rect.centerY() / 1000;
+
+				//Spring damping system
+				int linearAcc = 64 * (faces[0].rect.height() - currDesiredHeight) / currDesiredHeight;
+				int linearSpringConst = 1;
+				int angularSpringConst = 1;
+				int angularAcc = faceYPos;
+
+
+				int dampingNumerator = 1;
+				int dampingDenominator = 1;
+
+
+				leftSpeed = leftSpeed 
+						+   linearAcc * linearSpringConst
+						+	angularAcc * angularSpringConst
+						-	dampingNumerator * leftSpeed
+						/ dampingDenominator;
+
+				rightSpeed = rightSpeed 
+						+   linearAcc * linearSpringConst
+						-	angularAcc * angularSpringConst
+						-	dampingNumerator * rightSpeed
+						/ dampingDenominator;
+
+
+				//direct speed
+				//				int change = 64 * (faces[0].rect.height() - currDesiredHeight)/currDesiredHeight;
+				//
+				//				if (change < 0) //strange hack! reverse speed reflect in a slower real speed (compared to forward)
+				//					change = change*4;
+				//
+				//				//Calculate face rectangle area:
+				//				Log.d(TAG, "change:" + change);
+
+				// Speed ranges from -127 to 127, but only use speed ranges from -64 to 64:
+				//				int l = faceYPos;
+				//				int r = -faceYPos; 
+				//
+				//				leftSpeed = l + change;
+				//				rightSpeed = r + change;
+
+				//					Log.d(TAG, "Left=: " + faceYPos * 127 / 1000); 
+				//					Log.d(TAG, "Right=: " + (-faceYPos * 127 / 1000));
+			} else {
+				leftSpeed = 0;
+				rightSpeed = 0;
+				currDesiredHeight = 0;
+			}
+
+			wheelphone.setSpeed(leftSpeed, rightSpeed);
+			updateOutput();
+		}
+	};
+}
