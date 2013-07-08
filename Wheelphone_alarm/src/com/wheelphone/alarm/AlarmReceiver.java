49550b4071fb1ed43b20cad1e81bb2315483bec8
gnicolax@gmail.com <gnicolax@gmail.com@69f22f91-4d63-1a31-ada6-6e98f0020068>
2013-07-08 13:26:53 +0000

    NV cleanup

--- Wheelphone_alarm/src/com/wheelphone/alarm/AlarmReceiver.java
+++ Wheelphone_alarm/src/com/wheelphone/alarm/AlarmReceiver.java
@@ -9,7 +9,6 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.PowerManager;
 import android.util.Log;
 import android.widget.Toast;
