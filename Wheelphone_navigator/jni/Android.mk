df78fc8047b692a1fa03218864aac14e37be31ac
gnicolax@gmail.com <gnicolax@gmail.com@69f22f91-4d63-1a31-ada6-6e98f0020068>
2013-08-23 15:46:21 +0000

    OF cleanup

--- Wheelphone_navigator/jni/Android.mk
+++ Wheelphone_navigator/jni/Android.mk
@@ -9,14 +9,16 @@ include ~/Dropbox/wheelphone/sample_code/OpenCV-2.4.6-android-sdk/sdk/native/jni
 
 LOCAL_SRC_FILES  := \
 	com_wheelphone_navigator_helpers_MotionTrackerNativeWrapper.cpp \
-	attractor.cpp \
-	opticalFlow_FB.cpp
-#	opticalFlow_LK.cpp \
+	opticalFlow_LK.cpp
+#	attractor.cpp \
+#	opticalFlow_FB.cpp
 
 	
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_LDLIBS     += -llog -ldl
 
 LOCAL_MODULE     := obstacles_tracker
+
+LOCAL_CFLAGS += -std=c++11
 
 include $(BUILD_SHARED_LIBRARY)
