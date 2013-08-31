b0d2973e2cf4e293a18abdccb411dc69a2e1b4a9
gnicolax@gmail.com <gnicolax@gmail.com@69f22f91-4d63-1a31-ada6-6e98f0020068>
2013-08-31 12:08:56 +0000

    updated Fullscreen code

--- Wheelphone_faceme/src/com/wheelphone/faceme/ActivityMain.java
+++ Wheelphone_faceme/src/com/wheelphone/faceme/ActivityMain.java
@@ -1,7 +1,15 @@
 package com.wheelphone.faceme;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentManager.OnBackStackChangedListener;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -12,8 +20,11 @@ public class ActivityMain extends Activity {
 	private static final String TAG = ActivityMain.class.getName();
 
 	private View mContentView;
+	private View mControlsView;
 
 	private Fullscreen mFullscreen;
+
+	private FragmentFaceme mFragmentNavigator;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +38,46 @@ public class ActivityMain extends Activity {
 
 		setContentView(R.layout.activity_main);
 
-		getFragmentManager().beginTransaction().replace(R.id.content_frame, new FragmentFaceme()).commit();
+		mFragmentNavigator = new FragmentFaceme();
+//		fragment.setRetainInstance(true);
+		getFragmentManager().beginTransaction().replace(R.id.frame_preview, mFragmentNavigator).commit();
 
-		mContentView = findViewById(R.id.content_frame);
+		mContentView = findViewById(R.id.frame_preview);
+		mControlsView = findViewById(R.id.frame_controls);
 		
-		mFullscreen = new Fullscreen(mContentView, getActionBar());
+		mFullscreen = new Fullscreen(mContentView, mControlsView, getActionBar());
 
+	}    
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Fragment fragment = null;
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_about:
+	        	Log.d(TAG, "about");
+	        	fragment = new FragmentAbout();
+	        	break;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+		if(fragment != null){
+			mFullscreen.hideActionBar();
+			mControlsView.setVisibility(View.GONE);
+			FragmentManager fragmentManager = getFragmentManager();
+			fragmentManager.addOnBackStackChangedListener(mOnBackStackChangedListener);
+			FragmentTransaction transaction = fragmentManager.beginTransaction();
+			transaction.addToBackStack(null).replace(R.id.frame_preview, fragment).commit();
+		}
+		return true;
 	}
 	
 	@Override
@@ -40,4 +85,14 @@ public class ActivityMain extends Activity {
 		super.onResume();
 		mFullscreen.resume();
 	}
+	
+	private  OnBackStackChangedListener mOnBackStackChangedListener = new OnBackStackChangedListener() {    
+        public void onBackStackChanged() {
+        	Log.d(TAG, "getFragmentManager().getBackStackEntryCount(): " + getFragmentManager().getBackStackEntryCount());
+            if (getFragmentManager().getBackStackEntryCount() == 0) {
+            	Log.d(TAG, "back to initial fragment");
+            	mFullscreen.show();
+            }
+        }
+    };
 }
--- Wheelphone_faceme/src/com/wheelphone/util/Fullscreen.java
+++ Wheelphone_faceme/src/com/wheelphone/util/Fullscreen.java
@@ -1,52 +1,68 @@
 package com.wheelphone.util;
 
+
 import android.app.ActionBar;
 import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class Fullscreen {
 	private static final String TAG = Fullscreen.class.getName();
 
 	private View mContentView;
+	private View mControlsView;
 	private static final int HIDE_DELAY = 3000;
-	private boolean mIsHidden = false;
+	private volatile boolean mIsHidden = false;
 
 	private ActionBar mActionBar;
 
-	public Fullscreen(View contentView, ActionBar actionBar) {
+	public Fullscreen(View contentView, View controlsView, ActionBar actionBar) {
 		mActionBar = actionBar;
 		mContentView = contentView;
+		mControlsView = controlsView;
 		mContentView.setOnSystemUiVisibilityChangeListener(mSystemUiVisibilityChangeListener);
 
 		// Set up the user interaction to manually show or hide the system UI.
-		mContentView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (mIsHidden){
-					show();
-				}
-			}
-		});
+		mContentView.setOnTouchListener(mTouchListener);
 	}
+	
+	private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+		@Override
+		public boolean onTouch(View view, MotionEvent event) {
+			if (event.getAction() != MotionEvent.ACTION_DOWN)
+				return false;
+			
+			if (!mIsHidden)
+				hide();
+			else
+				show();
+			return false;
+		}
+	};
+
 	public void resume() {
 		delayedHide(HIDE_DELAY);
 	}
-
+	
 	private synchronized void hide() {
 		if (mIsHidden)
 			return;
-		mActionBar.hide();
-		mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 		mIsHidden = true;
+		mHideHandler.removeCallbacks(mHideRunnable);
+		mActionBar.hide();
+		mControlsView.setVisibility(View.GONE);
+		mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//		Log.d(TAG, "hide: " + mIsHidden);
 	}
 
 	public synchronized void show() {
 		if (!mIsHidden)
 			return;
-		mActionBar.show();
-		mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
 		mIsHidden = false;
-		delayedHide(HIDE_DELAY);
+		mActionBar.show();
+		mControlsView.setVisibility(View.VISIBLE);
+		mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+//		Log.d(TAG, "show: " + mIsHidden);
 	}
 
 	/*
@@ -55,13 +71,15 @@ public class Fullscreen {
 	private View.OnSystemUiVisibilityChangeListener mSystemUiVisibilityChangeListener = new View.OnSystemUiVisibilityChangeListener() {
 		@Override
 		public void onSystemUiVisibilityChange(int vis) {
-//			Log.d(TAG, "onSystemUiVisibilityChange: " + ((vis & (View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)) == 0));
-//			Log.d(TAG, "onSystemUiVisibilityChange2: " + ((vis & View.SYSTEM_UI_FLAG_LOW_PROFILE) == 0));
+//			Log.d(TAG, "View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION: " + (View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION));
+//			Log.d(TAG, "View.SYSTEM_UI_FLAG_LOW_PROFILE: " + (View.SYSTEM_UI_FLAG_LOW_PROFILE));
+//			Log.d(TAG, "View.SYSTEM_UI_FLAG_VISIBLE: " + (View.SYSTEM_UI_FLAG_VISIBLE));
 //			Log.d(TAG, "vis: " + vis);
 
-			if (((vis & (View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)) == 0) || (vis == View.SYSTEM_UI_FLAG_LOW_PROFILE)) {
+			//returned from having the navigation controls hidden, so show the rest of the UI:
+			if ((vis == View.SYSTEM_UI_FLAG_LOW_PROFILE) || (vis == View.SYSTEM_UI_FLAG_VISIBLE)) {
 				show();
-			}
+			} 
 		}
 	};
 
